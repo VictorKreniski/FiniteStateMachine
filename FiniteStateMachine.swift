1512ec4b9020824b463540136a6d75ca92e373fe
Victor Kreniski <krevi2796@gmail.com>
2018-02-19 14:23:49 -0300

    Adding FiniteStateMachine.swift file to repository

---
+++ FiniteStateMachine.swift
@@ -0,0 +1,80 @@
+//
+//  FiniteStateMachine.swift
+//  TerceiroMiniChallenge
+//
+//  Created by Victor Oliveira Kreniski on 16/11/17.
+//  Copyright © 2017 Victor Oliveira Kreniski. All rights reserved.
+//
+
+import Foundation
+
+/**
+ 
+ This class is a generic representation of finite state machine
+ - Important: State hashable parameter MUST BE the Hashable with all the possible states (Examples: Idle, Jump )
+ - Important: ActionTypes hashable parameter MUST BE the hashable with all the Action with minemonic names (Example: IdleToJump)
+ - Author: Victor Kreniski
+ */
+public final class FiniteStateMachine<State: Hashable, ActionTypes: Hashable> {
+    
+    //MARK: - Properties
+    // The actual state in some moment will be saved here
+    public private(set) var currentState: State
+    // The array with all possibles actions
+    private var actions = [State:[ActionTypes:State]]()
+    
+    //MARK: - Init
+    /**
+     Class Init with the initial state
+     - parameters:
+        - initialState: State object representing the first state.
+     */
+    public init(initialState: State) {
+        self.currentState = initialState
+    }
+    
+    //MARK: - Methods
+    /**
+     addAction is a method able to add the possible action on the actions hash array
+     - parameters:
+        - action: Action hash object.
+        - first: First State object
+        - second: Second State object
+     */
+    public final func addAction(_ action: ActionTypes, from first: State, to second: State) {
+        //checks if the action dictionary in the first state is nil
+        if actions[first] == nil {
+            actions[first] = [:]
+        }
+        actions[first]?[action] = second
+    }
+    
+    /**
+     This method returns a answer if the action can advance
+     - parameters:
+        - action: Action hash object.
+     */
+    public final func canAdvanceTo(action: ActionTypes) -> Bool {
+        return actions[self.currentState]?[action] != nil
+    }
+    
+    // Type aliases allow developers to define synonyms for pre-existing types.
+    // Simple typealias
+    public typealias CompletionHandler = (_ old: State,_ new: State) -> ()
+    /**
+     This method will make you advance into the next action, if possible.
+     - parameters:
+        - action: Action hash object.
+        - completition: CompletionHandler
+        - returns: State Object
+     */
+    public final func advanceTo(action: ActionTypes, completion: CompletionHandler? = nil){
+        let previousState = self.currentState
+        if let nextState = actions[previousState]?[action], nextState != previousState {
+            self.currentState = nextState
+            completion?(previousState, nextState)
+        }
+    }
+    
+}
+
